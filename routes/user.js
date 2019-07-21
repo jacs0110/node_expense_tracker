90aef37404ef38b591f932ef0fff010ca1d96035
jacs <wbruce0110@gmail.com>
2019-07-21 19:08:08 +0800

    add bcrypt

--- routes/user.js
+++ routes/user.js
@@ -40,12 +40,20 @@ router.post('/register', (req, res) => {
         email,
         password,
       })
-      newUser
-        .save()
-        .then(user => {
-          res.redirect('/')
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+
+          newUser
+            .save()
+            .then(user => {
+              res.redirect('/')
+            })
+            .catch(err => console.log(err))
         })
-        .catch(err => console.log(err))
+      })
     } else {
       console.log('Password inconsistent')
       res.render('register', {
