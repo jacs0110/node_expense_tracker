10284a21942d5eb10e884a51ad150aa76a0cfc5d
jacs <wbruce0110@gmail.com>
2019-07-24 00:28:08 +0800

    update query

--- routes/home.js
+++ routes/home.js
@@ -11,11 +11,11 @@ router.get('/', authenticated, async (req, res) => {
   console.log(`welcome to home page, user ${req.user.id}`)
   try {
 
-    let queryUser = `WHERE Records.UserId=${req.user.id}`
+    let queryUser = `WHERE "Records"."UserId"=${req.user.id}`
     console.log(queryUser)
 
     // query records
-    let rawRecords = await db.sequelize.query(`SELECT Records.id,Records.date,Records.name,Records.amount,Records.CategoryId, Categories.categoryName,Categories.icon FROM Records JOIN Categories ON Records.CategoryId = Categories.id ${queryUser} ORDER BY Records.date DESC`)
+    let rawRecords = await db.sequelize.query(`SELECT "Records"."id","Records"."date","Records"."name","Records"."amount","Records"."CategoryId", "Categories"."categoryName","Categories"."icon" FROM "Records" JOIN "Categories" ON "Records"."CategoryId" = "Categories"."id" ${queryUser} ORDER BY "Records"."date" DESC`)
 
     rawRecords[0].forEach(element => {
       element.date = element.date.toISOString().split("T")[0]
@@ -30,7 +30,7 @@ router.get('/', authenticated, async (req, res) => {
     })
 
     //query month list
-    let rawMonths = await db.sequelize.query(`SELECT Records.date FROM Records ${queryUser} GROUP BY Records.date ORDER BY Records.date DESC`)
+    let rawMonths = await db.sequelize.query(`SELECT "Records"."date" FROM "Records" ${queryUser} GROUP BY "Records"."date" ORDER BY "Records"."date" DESC`)
 
     rawMonths[0].forEach(element => {
       let year = element.date.toISOString().split("-")[0]
@@ -41,7 +41,7 @@ router.get('/', authenticated, async (req, res) => {
     const monthList = [... new Set(rawMonths[0].map(x => x.date))]
 
     //query total amount
-    let totalAmount = await db.sequelize.query(`SELECT SUM(Records.amount) as sum FROM Records JOIN Categories ON Records.CategoryId = Categories.id ${queryUser}`)
+    let totalAmount = await db.sequelize.query(`SELECT SUM("Records"."amount") as "sum" FROM "Records" JOIN "Categories" ON "Records"."CategoryId" = "Categories"."id" ${queryUser}`)
 
     return res.render('index', { records: rawRecords[0], categoryList: categoryList, totalAmount: totalAmount[0][0], monthList: monthList })
   } catch (e) {
